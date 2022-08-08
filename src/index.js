0a53559f490770e2d1b0c2dcd275f38638bb2ad5
vigo44 <vvigo@rambler.ru>
2022-08-09 00:16:58 +0400

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function toReadable (number) {
-  
-}
+    let unitsArr = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tensArr = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety' ];
+    let result = '';
+    let units = (number%10)? (' ' + (unitsArr[number%10])): '';
+    let hundred = ((number/100)^0)? unitsArr[((number/100)^0)] + ' hundred': '';
+    let tens = ((number%100)<20)? unitsArr[(number%100)]: tensArr[(((number%100)/10)^0)] + units;
+    result = hundred + ((hundred.length>0)&&(tens.length>0)? ' ': '') + tens;
+    if (number==0) return 'zero';
+    return (result);
+  }
